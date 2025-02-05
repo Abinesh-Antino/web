3335a7f02701d5372d4e5e5a3c77effe68132606
Abinesh Jain <abinesh@antino.com>
2025-02-05 18:08:32 +0530

    refactor: update HTML structure and remove unused CSS styles

--- script.js
+++ script.js
@@ -1,179 +1,162 @@
-// Initialize tooltips for the buttons after the DOM is loaded
-document.addEventListener("DOMContentLoaded", function () {
+const BASEURL = 'https://67a0fda298c6dba5af94.appwrite.global';
+
+document.addEventListener("DOMContentLoaded", () => {
     initializeTooltips();
+    setupClipboardPaste();
+    setupAutoPaste();
+    populateTable();
+    setupShortenButton();
+    setUpTheme();
+    getData();
 });
 
-// Select UI elements
-const recordBtn = document.getElementById("recordBtn");
-const durationDisplay = document.getElementById("duration");
-const audioClips = document.getElementById("audioClips");
+const linksData = [];
 
-let mediaRecorder;
-let audioChunks = [];
-let timerInterval;
-let elapsedTime = 0; // Time in seconds
 
-// Initialize media recorder
-async function initMediaRecorder() {
-    try {
-        const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-        mediaRecorder = new MediaRecorder(stream);
-        setUpMediaRecorderEvents();
-    } catch (error) {
-        console.error("Error accessing microphone:", error);
-        alert("Error accessing microphone: " + error);
-    }
-}
-
-// Set up media recorder events
-function setUpMediaRecorderEvents() {
-    mediaRecorder.onstart = startRecording;
-    mediaRecorder.ondataavailable = collectAudioChunks;
-    mediaRecorder.onstop = stopRecording;
-}
-
-// Start recording
-function startRecording() {
-    audioChunks = [];
-    elapsedTime = 0;
-    updateRecordButtonState('stop');
-    startTimer();
-}
-
-// Collect audio chunks
-function collectAudioChunks(event) {
-    audioChunks.push(event.data);
-}
-
-// Stop recording
-function stopRecording() {
-    clearInterval(timerInterval); // Stop the timer when recording stops
-    updateRecordButtonState('start');
-    resetDurationDisplay();
-    createAudioClip();
-}
-
-// Start the timer and update the duration display
-function startTimer() {
-    timerInterval = setInterval(() => {
-        elapsedTime++;
-        const minutes = Math.floor(elapsedTime / 60);
-        const seconds = elapsedTime % 60;
-        durationDisplay.textContent = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
-    }, 1000);
-}
-
-// Update the record button text and icon
-function updateRecordButtonState(state) {
-    if (state === 'stop') {
-        recordBtn.innerHTML = '<i class="bi bi-mic-mute fs-1 d-block"></i> Stop Recording';
-    } else {
-        recordBtn.innerHTML = '<i class="bi bi-mic fs-1 d-block"></i> Start Recording';
-    }
-}
-
-// Reset the duration display
-function resetDurationDisplay() {
-    durationDisplay.textContent = "00:00"; // Reset the duration display
-}
-
-// Create the audio clip with controls and buttons
-function createAudioClip() {
-    const audioBlob = new Blob(audioChunks, { type: "audio/wav" });
-    const audioUrl = URL.createObjectURL(audioBlob);
-
-    const audio = createAudioElement(audioUrl);
-    const listItem = createListItem(audio, audioUrl);
-
-    audioClips.appendChild(listItem);
-}
-
-// Create an audio element with the given URL
-function createAudioElement(audioUrl) {
-    const audio = document.createElement("audio");
-    audio.src = audioUrl;
-    audio.controls = true;
-    audio.load();
-    return audio;
-}
-
-// Create a list item with the audio and action buttons
-function createListItem(audio, audioUrl) {
-    const listItem = document.createElement("li");
-    listItem.classList.add("list-group-item", "d-flex", "justify-content-between", 'align-items-center');
-    listItem.appendChild(audio);
-
-    const buttonsContainer = createButtonsContainer(audioUrl);
-    listItem.appendChild(buttonsContainer);
-
-    return listItem;
-}
-
-// Create the buttons container with delete and download buttons
-function createButtonsContainer(audioUrl) {
-    const buttonsContainer = document.createElement("div");
-    buttonsContainer.classList.add("d-flex", "align-items-center");
-
-    const deleteBtn = createDeleteButton(audioUrl);
-    const downloadBtn = createDownloadButton(audioUrl);
-
-    buttonsContainer.appendChild(deleteBtn);
-    buttonsContainer.appendChild(downloadBtn);
-
-    return buttonsContainer;
-}
-
-// Create the delete button
-function createDeleteButton(audioUrl) {
-    const deleteBtn = document.createElement("button");
-    deleteBtn.classList.add("btn", "btn-danger", "ms-2");
-    deleteBtn.innerHTML = '<i class="bi bi-trash"></i>'; // Trash icon
-    deleteBtn.addEventListener("click", () => {
-        deleteAudioClip(deleteBtn, audioUrl);
+function getData() {
+    fetch(BASEURL).then(response => response.json()).then(data => {
+        const urls = data.urls;
+        const documents = urls['documents'] ?? [];
+        populateTable(documents);
+        showToast('Data fetched !');
+    }).catch(error => {
+        console.error(error);
+        showToast('Something went wrong');
     });
-    return deleteBtn;
 }
 
-// Create the download button
-function createDownloadButton(audioUrl) {
-    const downloadBtn = document.createElement("button");
-    downloadBtn.classList.add("btn", "btn-success", "ms-2");
-    downloadBtn.innerHTML = '<i class="bi bi-download"></i>'; // Download icon
-    downloadBtn.addEventListener("click", () => {
-        downloadAudio(audioUrl);
+function createLink(url) {
+    fetch(BASEURL, {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ url })
+    }).then(response => response.json()).then(data => getData()).catch(error => {
+        console.error(error);
+        showToast('Something went wrong');
     });
-    return downloadBtn;
 }
 
-// Delete the audio clip and release memory
-function deleteAudioClip(deleteBtn, audioUrl) {
-    const listItem = deleteBtn.closest("li");
-    listItem.remove(); // Remove the audio clip from the list
-    URL.revokeObjectURL(audioUrl); // Release the object URL to free memory
-}
-
-// Download the audio clip
-function downloadAudio(audioUrl) {
-    const a = document.createElement("a");
-    a.href = audioUrl;
-    a.download = "audio_clip.wav";
-    a.click(); // Trigger download
-}
-
-// Initialize tooltips for the buttons
+/**
+ * Initializes Bootstrap tooltips.
+ */
 function initializeTooltips() {
-    const tooltipElements = document.querySelectorAll('[data-bs-toggle="tooltip"]');
-    tooltipElements.forEach(function (element) {
-        new bootstrap.Tooltip(element);
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+    [...tooltipTriggerList].forEach(tooltip => new bootstrap.Tooltip(tooltip));
+}
+
+/**
+ * Handles clipboard pasting when the user clicks the clipboard icon.
+ */
+async function pasteFromClipboard() {
+    try {
+        const text = await navigator.clipboard.readText();
+        document.getElementById("link").value = text;
+    } catch (err) {
+        console.error("Failed to read clipboard: ", err);
+    }
+}
+
+/**
+ * Sets up event listener for clipboard pasting on button click.
+ */
+function setupClipboardPaste() {
+    const clipboardBtn = document.querySelector(".input-group-text");
+    if (clipboardBtn) {
+        clipboardBtn.addEventListener("click", pasteFromClipboard);
+    }
+}
+
+/**
+ * Handles auto-pasting from the clipboard when the toggle switch is enabled.
+ */
+async function autoPaste() {
+    const autoPasteSwitch = document.querySelector(".form-check-input");
+    if (autoPasteSwitch.checked) {
+        await pasteFromClipboard();
+    }
+}
+
+/**
+ * Sets up event listener for auto-pasting on switch toggle.
+ */
+function setupAutoPaste() {
+    const autoPasteSwitch = document.querySelector(".form-check-input");
+    if (autoPasteSwitch) {
+        autoPasteSwitch.addEventListener("change", autoPaste);
+    }
+}
+
+/**
+ * Populates the table dynamically from `linksData`.
+ */
+function populateTable(data = []) {
+    const tbody = document.querySelector("tbody");
+    tbody.innerHTML = "";
+
+    data.forEach((link, index) => {
+        const row = document.createElement("tr");
+        row.innerHTML = `
+            <td scope="row">${index + 1}</td>
+            <td><a href="${BASEURL}/${link.$id}" target="_blank">${link.$id}</a></td>
+            <td><a href="${link.url}" target="_blank">${link.url}</a></td>
+            <td>${link.clicks ?? 0}</td>
+            <td>${link.$createdAt}</td>
+        `;
+        tbody.appendChild(row);
     });
 }
 
-// Toggle start/stop recording
-recordBtn.addEventListener("click", async () => {
-    if (mediaRecorder && mediaRecorder.state === "recording") {
-        mediaRecorder.stop();
-    } else {
-        if (!mediaRecorder) await initMediaRecorder();
-        mediaRecorder.start();
-    }
-});
+/**
+ * Handles the form submission and adds a new shortened link to the table.
+ */
+function setupShortenButton() {
+    const shortenButton = document.getElementById("shorten");
+    const linkInput = document.getElementById("link");
+
+    shortenButton.addEventListener("click", (event) => {
+        const originalUrl = linkInput.value.trim();
+        if (!originalUrl) {
+            showToast('Please enter a valid URL ');
+            return;
+        }
+
+        createLink(originalUrl);
+
+        // Clear input field
+        linkInput.value = "";
+    });
+}
+
+function setUpTheme() {
+    const lightBtn = document.getElementById("light");
+    const darkBtn = document.getElementById("dark");
+
+    lightBtn.addEventListener("click", () => {
+        toggleTheme("light");
+        lightBtn.classList.add("active");
+        darkBtn.classList.remove("active");
+        showToast("Theme set to light");
+    });
+    darkBtn.addEventListener("click", () => {
+        toggleTheme("dark");
+        darkBtn.classList.add("active");
+        lightBtn.classList.remove("active");
+        showToast("Theme set to dark");
+    });
+
+}
+
+function showToast(message = "Hello") {
+    const toastEl = document.getElementById("toast");
+    toastEl.querySelector(".toast-body").textContent = message;
+    const toast = new bootstrap.Toast(toastEl);
+    toast.show();
+}
+
+function toggleTheme(theme) {
+    console.log(theme);
+
+    document.querySelector('html').attributes["data-bs-theme"].value = theme;
+}
